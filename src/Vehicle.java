9c833d30020ac0f1f9fdc85e152f0c648234983c
Timur Kaftaranov <kaftaranov@rambler.ru>
2022-06-18 16:18:16 +0600

    Second commit

---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    protected String modelName;
+    protected int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+}
